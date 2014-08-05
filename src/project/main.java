ab1ea4f4d9a0df90847ac84070be679592eaced1
undefined <miniim@ip-172-31-27-1.ap-northeast-1.compute.internal>
2014-08-05 04:08:10 +0000

    inherit to thread

--- src/project/main.java
+++ src/project/main.java
@@ -1,97 +1,37 @@
 package project;
-import java.util.Random;
-import java.util.Scanner;
-public class main{
 
+public class main {
+
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		Opreation[] Opreations = new Opreation[8];
-		System.out.println("input seed");
-		Scanner sc= new Scanner(System.in);
-		int seed=sc.nextInt();
+		// TODO Auto-generated method stub
 
-		Random rd=new Random(seed);
+		MyThread t1= new MyThread("goorm1");
+		MyThread t2= new MyThread("goorm2");
+		MyThread t3= new MyThread("goorm3");
+
+		t1.start();
+		t2.start();
+		t3.start();
 		
-		
-		for(int i=0;i<Opreations.length;i++){
-			int random=Math.abs(rd.nextInt()%4) ;
-			switch(random){
-				case 0:
-					Opreations[i] = new Add(rd.nextInt()%100,rd.nextInt()%100);
-					break;
-				case 1:
-					Opreations[i] = new Minus(rd.nextInt()%100,rd.nextInt()%100);
-					break;
-				case 2:
-					Opreations[i] = new Mul(rd.nextInt()%100,rd.nextInt()%100);
-					break;
-				case 3:
-					Opreations[i] = new Div(rd.nextInt()%100,rd.nextInt()%100);
-					break;
-			}
+	}
+
+}
+
+class MyThread extends Thread{
+	static int total=0;
+	int i=0;
+	String threadName="";
+	public MyThread(String str){
+		this.threadName=str;
+	}
+	public void run(){
+		for(i=0;i<50;i++){
+			System.out.println(this.threadName+" : "+i);
+			total+=i;
 		}
-
-		for(int i=0;i<Opreations.length;i++){
-			System.out.printf(Opreations[i].x+" "+Opreations[i].getClass().getSimpleName()+" "+Opreations[i].y+" result :");
-			System.out.println(Opreations[i].op());
-			
-		}
-	}
-
-}abstract class Opreation{
-	int x,y;
-	public abstract int op();
-
-}
-
-class Add extends Opreation{
-	public Add(int x,int y){
-		this.x=x;
-		this.y=y;
-	}
-	
-	@Override
-	public int op() {
-		return x+y;
-	}
-}
-
-class Minus extends Opreation{
-	
-	public Minus(int x,int y){
-		this.x=x;
-		this.y=y;
-	}
-
-	@Override
-	public int op() {
-		return x-y;
-	}
-}
-
-
-class Mul extends Opreation{
-	
-	public Mul(int x,int y){
-		this.x=x;
-		this.y=y;
-	}
-
-	@Override
-	public int op() {
-		return x*y;
-	}
-}
-
-
-class Div extends Opreation{
-	
-	public Div(int x,int y){
-		this.x=x;
-		this.y=y;
-	}
-
-	@Override
-	public int op() {
-		return x/y;
+		System.out.println(this.threadName+" end. total is "+total);
 	}
 }
